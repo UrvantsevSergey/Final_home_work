db92a02beef87396d06b520ba358ccaed394fb08
UrvantsevSergey <159543075+UrvantsevSergey@users.noreply.github.com>
2024-04-08 10:23:58 +0000

    Create_array

--- main.cs
+++ main.cs
@@ -1,7 +1,19 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+class Program 
+{
+  public static void Main (string[] args) 
+  {
+    Console.WriteLine("Введите количество элементов: ");
+    int size = Int32.Parse(Console.ReadLine());
+    string[] array = new string[size];
+    FillArray(array);
+    PrintArray(array);
+    int mas_size = SizeArray(array);
+
+    string[] mas = new string[mas_size];
+    Console.WriteLine();
+    PrintArray(Metod(array, mas));
+    
   }
 }
